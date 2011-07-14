83d6c86df22f115d423e14517152ae6a56268425
Scott Payne <github@timmyxaphan.fastmail.fm>
2011-07-14 19:34:02 +1000

    Changed PHPWebdriverCapsTest to use new ini file.

--- trunk/test/PHPWebdriverCapsTest.php
+++ trunk/test/PHPWebdriverCapsTest.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'phpwebdriver/WebDriver.php';
+require_once dirname(__FILE__) . '/../phpwebdriver/WebDriver.php';
 
 /**
  * 
@@ -10,10 +10,24 @@ require_once 'phpwebdriver/WebDriver.php';
  */
 class PHPWebDriverCapsTest extends PHPUnit_Framework_TestCase {
 
-    private $test_url = "http://localhost:8080/php-webdriver-bindings/test_page.php";
+    private $config;
+    private $test_url;
+
+    public function __construct()
+    {
+        parent::__construct(func_get_args());
+        $config = parse_ini_file(dirname(__FILE__) . '/../config/test.ini', true);
+        if ($config) {
+            $this->config = $config;
+        } else {
+            throw new RuntimeException("Couldn't parse test.ini file");
+        }
+        $this->test_url = $this->config['test_page']['url'];
+    }
 
     protected function setUp() {
-        $this->webdriver = new WebDriver("localhost", 4444);
+        $selenium = $this->config['selenium'];
+        $this->webdriver = new WebDriver($selenium['host'], $selenium['port']);
         $this->webdriver->connect("android", "", array(
 			"platform"=>"ANDROID",
 			"browserConnectionEnabled"=>true,
